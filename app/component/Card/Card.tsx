ae3b308e8c6e96ce389ea698222b429114281a59
aliciviltech <ali.civiltech@gmail.com>
2024-10-26 11:53:44 +0500

    3rd commit

--- app/component/Card/Card.tsx
+++ app/component/Card/Card.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
+interface suitInterface {
+  category: string,
+  title: string,
+  price: number,
+  material: string,
+  discount: number | null,
+  instock: boolean,
+  image: string
+}
 
 
-
-const Card = ({suit}:{suit:any}) => {
+const Card = ({ suit }: { suit: suitInterface }) => {
   return (
     <div className="cardContainer w-[300px]">
-        <div className="image">
-            <img className='w-[300px]' src={suit.image} alt="" />
-        </div>
-        <div className="description">
-            <div>{suit.title}</div>
-            <p>Price: {suit.price}</p>
-        </div>
+      <div className="image">
+        <img className='w-[300px]' src={suit.image} alt="" />
+      </div>
+      <div className="description">
+        <div>{suit.title}</div>
+        <p>Price: {suit.price}</p>
+      </div>
     </div>
   )
 }
